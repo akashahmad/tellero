d830ac8889cc5f56254aa6158c3ba157c9ea4804
akashahmad <engr.akashahmad>
2021-06-18 04:41:53 +0500

    static pages convertion in progress

---
+++ assets/script/financial-year.js
@@ -0,0 +1,7 @@
+// kob form data
+$(document).ready(function () {
+    $("#metode").select2({
+        placeholder: "Netto",
+        allowClear: true,
+    });
+})
---
+++ assets/script/kob.js
@@ -0,0 +1,19 @@
+// kob form data
+$(document).ready(function () {
+    $("#bought-from").select2({
+        placeholder: "Denmark",
+        allowClear: true,
+    });
+    $("#beholdning").select2({
+        placeholder: "--- Vaelg beholdning ---",
+        allowClear: true,
+    });
+    $("#valute").select2({
+        placeholder: "Danske kroner (DKK)",
+        allowClear: true,
+    });
+    $("#konto").select2({
+        placeholder: "--- Vaelg beholdning ---",
+        allowClear: true,
+    });
+})
