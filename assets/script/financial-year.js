1915e4906644916ec3c38598d431ad9ec8849d4a
akashahmad <engr.akashahmad@gmail.com>
2021-06-29 05:05:12 +0500

    Changes complete

--- assets/script/financial-year.js
+++ assets/script/financial-year.js
@@ -1,25 +1,40 @@
 // kob form data
-$(document).ready(function () {
-  $("#metode").select2({
-    placeholder: "Netto",
-    allowClear: true,
-  });
-  $("#Vmlg").select2({
-    placeholder: "-- Vmlg bling --",
-    allowClear: true,
-  });
-  $("#Vmlg2").select2({
-    placeholder: "-- Vmlg bling --",
-    allowClear: true,
-  });
-  $("#Vmlg3").select2({
-    placeholder: "-- Vmlg bling --",
-    allowClear: true,
-  });
-  $(document).ready(function () {
-    $("#financial_more_content_button").click(function () {
-      $("#financial-more-content").toggle();
+$(document).ready(function() {
+    $("#metode").select2({
+        placeholder: "Netto",
+        allowClear: true,
     });
-  });
+    $("#Vmlg").select2({
+        placeholder: "-- Vmlg bling --",
+        allowClear: true,
+    });
+    $("#Vmlg2").select2({
+        placeholder: "-- Vmlg bling --",
+        allowClear: true,
+    });
+    $("#Vmlg3").select2({
+        placeholder: "-- Vmlg bling --",
+        allowClear: true,
+    });
+    $("#faktur-beholdning").select2({
+        placeholder: "Bank",
+        allowClear: true,
+    });
+    $("#faktur-konto").select2({
+        placeholder: "1000 - Salg of varer/ydelser m/moms",
+        allowClear: true,
+    });
+    $("#faktur-sprog").select2({
+        placeholder: "Dansh",
+        allowClear: true,
+    });
+    $("#faktur-valuta").select2({
+        placeholder: "Dansh kroner (DKK)",
+        allowClear: true,
+    });
+    $(document).ready(function() {
+        $("#financial_more_content_button").click(function() {
+            $("#financial-more-content").toggle();
+        });
+    });
 });
-
